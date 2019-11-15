61393fced5c3abbcbb2c1d87e9c387c422a13728
wawahuy <kakahuy99@gmail.com>
2019-11-15 23:50:20 +0700

    Model hoan thanh xu ly JSON

--- app/src/main/java/ml/huytools/ycnanswer/Model/CauHoi.java
+++ app/src/main/java/ml/huytools/ycnanswer/Model/CauHoi.java
@@ -1,6 +1,27 @@
 package ml.huytools.ycnanswer.Model;
 
-public class CauHoi {
+import ml.huytools.ycnanswer.Common.Model;
 
+public class CauHoi extends Model {
 
+    private String a;
+    private int c;
+    private int d;
+
+    public CauHoi() {
+        super();
+        a ="1";
+        c=2;
+        d=10;
+    }
+
+    @Override
+    protected Object GetAttributeValue(String attr) throws NoSuchFieldException, IllegalAccessException {
+        return this.getClass().getDeclaredField(attr).get(this);
+    }
+
+    @Override
+    public String[] GetAttributeExcept() {
+        return new String[]{"c"};
+    }
 }
