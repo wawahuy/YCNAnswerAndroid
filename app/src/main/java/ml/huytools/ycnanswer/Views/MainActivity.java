04fc73bca6e23c0873624b27ee48536f1d3b44d7
wawahuy <kakahuy99@gmail.com>
2019-11-24 07:58:43 +0700

    Api Provider complete Get, Post, Async

--- app/src/main/java/ml/huytools/ycnanswer/Views/MainActivity.java
+++ app/src/main/java/ml/huytools/ycnanswer/Views/MainActivity.java
@@ -3,22 +3,40 @@ package ml.huytools.ycnanswer.Views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import ml.huytools.ycnanswer.Commons.ApiProvider;
 import ml.huytools.ycnanswer.Commons.Model;
 import ml.huytools.ycnanswer.Models.CauHoi;
 import ml.huytools.ycnanswer.R;
 
 public class MainActivity extends AppCompatActivity {
+//////// NO Model - View - Presenter
+//////// UPDATE
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configAPI();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
+    }
+
+
+    void configAPI(){
+        /// Config Host
+        ApiProvider.SetHost("http://192.168.1.130:8000/api");
     }
 
 
